01b4e21932b0e674a9aa4cf73ae9417a3a0eb499
arthur360cgf <arthurmattei115@gmail.com>
2022-10-29 00:16:07 -0300

    colocando o sol

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -1,10 +1,11 @@
-import {
+import THREE, {
   Scene,
   AxesHelper,
   AmbientLight,
   DirectionalLight,
   Mesh,
   SphereGeometry,
+  BufferGeometry,
   MeshToonMaterial,
   PlaneGeometry,
   Color,
@@ -39,13 +40,57 @@ const PARAMS = {
   color: "#5EDCAE",
 }
 
-const sphere = new Mesh(
+const circulo = new Mesh(
   new SphereGeometry(0.75, 32, 32),
+  new MeshToonMaterial({
+    color: 0xffff00,
+    wireframe: false,
+  })
+)
+ 
+
+circulo.position.set(-5.8, 7.2, -6.7)
+circulo.castShadow = true
+
+const circuloCtrls = gui.addFolder({
+  title: "Sphere",
+})
+
+circuloCtrls.addInput(circulo.position, "x", {
+  label: "pos x",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+circuloCtrls.addInput(circulo.position, "y", {
+  label: "pos y",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+circuloCtrls.addInput(circulo.position, "z", {
+  label: "pos z",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+circuloCtrls.addInput(PARAMS, "color").on("change", (e) => {
+  sphere.material.color = new Color(e.value)
+})
+
+circuloCtrls.addInput(circulo.material, "wireframe")
+
+scene.add(circulo)
+
+
+const sphere = new Mesh(
+  new SphereGeometry(0.75, 37, 32),
   new MeshToonMaterial({
     color: new Color(PARAMS.color),
     wireframe: false,
   })
 )
+ 
 
 sphere.position.set(0, 2, 0)
 sphere.castShadow = true
@@ -79,6 +124,7 @@ sphereCtrls.addInput(PARAMS, "color").on("change", (e) => {
 sphereCtrls.addInput(sphere.material, "wireframe")
 
 scene.add(sphere)
+
 
 const plane = new Mesh(
   new PlaneGeometry(10, 10, 10, 10),
